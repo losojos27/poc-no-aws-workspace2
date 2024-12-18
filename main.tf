7d61c3ed8230df4383280a00f0cd40e1226c9748
Lee Owensby <lee.owensby@gmail.com>
2024-12-18 13:23:33 -0600

    fixed tfe_outputs

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ module "pet" {
 output "combined_pet_name" {
   sensitive   = false
   description = "Here is the name of your new pet!"
-  value       = [format("%s-%s", data.tfe_outputs.pet-name-workspace1.outputs.pet_name, module.pet.name)]
+  value       = [format("%s-%s", data.tfe_outputs.pet-name-workspace1.values.pet_name, module.pet.name)]
 
 }
 
